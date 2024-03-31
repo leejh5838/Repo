30878220891c692def8b5b2d289c562633e16d53
unknown <leejh5838@naver.com>
2024-03-31 21:56:58 +0900

    git test

---
+++ app/src/main/java/com/example/baseapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.baseapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Double result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+
+        edit1 = findViewById(R.id.Edit1);
+        edit2 = findViewById(R.id.Edit2);
+        btnAdd = findViewById(R.id.BtnAdd);
+        btnSub = findViewById(R.id.BtnSub);
+        btnMul = findViewById(R.id.BtnMul);
+        btnDiv = findViewById(R.id.BtnDiv);
+        textResult = findViewById(R.id.TextResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals("")) {
+                    textResult.setText("숫자를 입력하세요.");
+                    return;
+                }
+
+                result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result);
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals("")) {
+                    textResult.setText("숫자를 입력하세요.");
+                    return;
+                }
+
+                result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result);
+            }
+        });
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals("")) {
+                    textResult.setText("숫자를 입력하세요.");
+                    return;
+                }
+
+                result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result);
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if (num1.equals("") || num2.equals("")) {
+                    textResult.setText("숫자를 입력하세요.");
+                    return;
+                }
+
+                result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result);
+            }
+        });
+    }
+}
